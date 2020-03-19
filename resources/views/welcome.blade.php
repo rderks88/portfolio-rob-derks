1406dbf39b01c803a6b10aa6021a2277649a7268
Rob Derks <r.derks88@gmail.com>
2020-03-19 15:00:11 +0100

    check if git works as it should

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,8 @@
                     Laravel
                 </div>
 
+                hello
+
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
